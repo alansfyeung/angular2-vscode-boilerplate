eb0d3ba66fcec51bd7d69e5c4d0002ef6cf209b5
Alan Yeung <alan.yeung@btfinancialgroup.com>
2016-09-27 17:10:58 +1000

    Boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,34 @@ var sass = require('gulp-sass');
 var ts = require('gulp-typescript');
 var tsconf = require('./tsconfig.json');
 
-
+// Move all the .html files
 gulp.task('move', function() {
     gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist'))
 });
 
+// Compile specifically *.scss in that folder only
+// Expected you'll have a style.scss or a main.scss which
+// then includes ./partials/_partialfile.scss etc 
 gulp.task('compile-scss', function() {
     gulp.src('./src/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
 });
 
+// Compile .ts wherever it is, mapping out to the 
+// same path at the dest
 gulp.task('compile-typescript', function(){
     return gulp.src('./src/**/*.ts')
         .pipe(ts(tsconf.compilerOptions))
         .pipe(gulp.dest('dist'));
-        // .pipe(gulp.dest());     // presuming that outDir will prepend a folder path     
-        // function(f) {
-        //     console.log(f.base);
-        //     console.log(f.path);
-        //     // return f.base + '/../dist/' + f.path;
-        //     return './dist' + f.path.replace(f.base, '');
-        // }
 });
 
+//
+// Todo: Test runners could go here.
+//
+
+// Default task is watcher
 gulp.task('default', ['move', 'compile-scss', 'compile-typescript'], function() {
     gulp.watch('./src/**/*.html', ['move']);
     gulp.watch('./src/scss/*.scss', ['compile-scss']);
---
+++ readme.md
@@ -0,0 +1,24 @@
+## What is this?
+
+A slightly-opinionated but mostly [style guide compliant](https://angular.io/docs/ts/latest/guide/style-guide.html) boilerplate for [Angular 2](https://angular.io/).
+
+## Technology preferences
+
++ Uses [TypeScript](https://angular.io/docs/ts/latest/guide/style-guide.html) for Angular 2
++ SCSS for style sheets   
++ Gulp for task running
++ NPM is unavoidable in Angular 2
++ Follows Angular 2 defaults for things like [systemjs](https://github.com/systemjs/systemjs) <sup>1</sup>
++ [VS Code](https://code.visualstudio.com/Download) as the IDE<sup>2</sup>
+
+## Useful getting started links
+
++ [The style guide](https://angular.io/docs/ts/latest/guide/style-guide.html)
++ [One to Two](https://angular.io/docs/ts/latest/cookbook/a1-a2-quick-reference.html) side by side comparison, for jogging the memory
++ VS Code [tasks.json configuration](https://code.visualstudio.com/docs/languages/typescript)
++ [Gulp builtins](https://gulp.readme.io/docs/gulpsrcglobs-options)
++ [Tour of Heroes boilerplate](https://github.com/johnpapa/angular2-tour-of-heroes)
+
+<sup>1</sup> It would appear that SystemJS, Webpack, or Browserify are all valid module loaders for TypeScript
+
+<sup>2</sup> Built-in Typescript and SCSS syntax highlighting, amongst [other cool things](https://code.visualstudio.com/docs/languages/typescript)
