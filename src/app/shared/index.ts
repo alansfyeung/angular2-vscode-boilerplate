98c3b247c3c4dbc0670024f641acb40d324d9e3d
Alan Yeung <alan@creativebits.com.au>
2016-09-29 14:09:33 +1000

    Explain barrel file has become core module
    I thought I was going nuts when CTRL+F couldn't find the word barrel

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,1 +1,2 @@
 // This is a [barrel file](https://angular.io/docs/ts/latest/guide/style-guide.html#!#04-10)
+// Note: They don't call them [barrel files any more](https://github.com/angular/angular.io/commit/2b7fb868908527ba9fecb2e2c549143694aad363)
